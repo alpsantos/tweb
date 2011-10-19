c827eadae6386e2d215935256aefe2fe6cd4d367
Alessandro Santos <alpsantos@gmail.com>
2011-10-19 19:25:42 -0200

    listagem de prefeituras

--- TWeb.Administracao/Prefeituras/Detalhes.aspx.cs
+++ TWeb.Administracao/Prefeituras/Detalhes.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace TWeb.Administracao
+namespace TWeb.Administracao.Prefeituras
 {
-    public partial class Prefeituras : System.Web.UI.Page
+    public partial class Detalhes : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
---
+++ TWeb.Administracao/Prefeituras/Listagem.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TWeb.Administracao.Prefeituras
+{
+    public partial class Listagem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridViewListarPreituras_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes.Add("onmouseover", "this.style.backgroundColor='#ceedfc'");
+                e.Row.Attributes.Add("onmouseout", "this.style.backgroundColor=''");
+                e.Row.Attributes.Add("style", "cursor:pointer;");
+                e.Row.Attributes.Add("onclick", "location='detalhe.aspx?id=" + e.Row.Cells[0].Text + "'");
+            }
+        }
+
+    }
+}
